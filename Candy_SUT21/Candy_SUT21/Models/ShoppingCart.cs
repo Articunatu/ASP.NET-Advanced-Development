b2d636c6e78a397a8e08218a7de12318fb906f02
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-08-24 11:28:49 +0200

    feat: Change cart

--- Candy_SUT21/Candy_SUT21/Models/ShoppingCart.cs
+++ Candy_SUT21/Candy_SUT21/Models/ShoppingCart.cs
@@ -1,8 +1,10 @@
 ﻿using FluentAssertions.Common;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Candy_SUT21.Models
 {
@@ -10,7 +12,7 @@ namespace Candy_SUT21.Models
     {
         private readonly AppDbContext _appDbContext;
 
-        
+
         public string ShoppingCartID { get; set; }
         public List<ShoppingCartItem> ShoppingCartItems { get; set; }
 
@@ -28,6 +30,74 @@ namespace Candy_SUT21.Models
             session.SetString("CartID", cartId);
 
             return new ShoppingCart(context) { ShoppingCartID = cartId };
+        }
+
+        public void AddToCart(Candy candy, int amount)
+        {
+            var ShoppingCartItem = _appDbContext.ShoppingCartItems.SingleOrDefault(c => c.Candy.CandyId == candy.CandyId
+            && c.ShoppingCartID == ShoppingCartID);
+
+            if (ShoppingCartItem == null)
+            {
+                ShoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartID = ShoppingCartID,
+                    Candy = candy,
+                    Amount = amount
+                };
+                _appDbContext.ShoppingCartItems.Add(ShoppingCartItem);
+            }
+            else
+            {
+                ShoppingCartItem.Amount++;
+            }
+            _appDbContext.SaveChanges();
+        }
+
+        public int RemoveFromCart(Candy candy)
+        {
+            var ShoppingCartItem = _appDbContext.ShoppingCartItems.SingleOrDefault(c => c.Candy.CandyId == candy.CandyId
+            && c.ShoppingCartID == ShoppingCartID);
+
+            var localAmount = 0;
+
+            if (ShoppingCartItem != null)
+            {
+                if (ShoppingCartItem.Amount > 1)
+                {
+                    ShoppingCartItem.Amount--;
+                    localAmount = ShoppingCartItem.Amount;
+                }
+                else
+                {
+                    _appDbContext.ShoppingCartItems.Remove(ShoppingCartItem);
+                }
+            }
+
+            _appDbContext.SaveChanges();
+            return localAmount;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _appDbContext.ShoppingCartItems.
+                Where(c => c.ShoppingCartID == ShoppingCartID).
+                Include(s => s.Candy).ToList());
+        }
+
+        public void ClearCart()
+        {
+            var cartItems = _appDbContext.ShoppingCartItems.Where(c => c.ShoppingCartID == ShoppingCartID);
+            _appDbContext.ShoppingCartItems.RemoveRange(cartItems);
+            _appDbContext.SaveChanges();
+        }
+
+        public decimal GetShoppingCartTotal()
+        {
+            var total = _appDbContext.ShoppingCartItems.Where(c => c.ShoppingCartID == ShoppingCartID).
+                Select(c => c.Candy.Price * c.Amount).Sum();
+
+            return total;
         }
     }
 }
