4d8a80df2d6b5592cdda01d9bd28634a9620fe9b
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-09-12 14:47:52 +0200

    feat: One single model

---
+++ API-Angular-CRUD/API-Angular-CRUD/Models/Card.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Angular_CRUD.Models
+{
+    public class Card
+    {
+        [Key]
+        public Guid ID { get; set; }
+        public string HolderName { get; set; }
+        public string CardNumber { get; set; }
+        public int ExpireMonth { get; set; }
+        public int ExpireYear { get; set; }
+        public int CVC { get; set; }
+    }
+}
---
+++ API-Angular-CRUD/API-Angular-CRUD/Models/CardsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_Angular_CRUD.Models
+{
+    public class CardsDbContext : DbContext
+    {
+
+        public CardsDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Card> Cards { get; set; }
+    }
+}
